7d681fbb6cf17c457fc7661ef8442e12ef862d85
jw-park-github <jw.park.github@gmail.com>
2024-06-25 17:04:54 +0900

    update tokenprovider

--- src/main/java/com/devdoc/backend/security/TokenProvider.java
+++ src/main/java/com/devdoc/backend/security/TokenProvider.java
@@ -4,13 +4,13 @@ package com.devdoc.backend.security;
 import com.devdoc.backend.model.UserEntity;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+
 import io.jsonwebtoken.security.Keys;
-import lombok.extern.slf4j.Slf4j;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
+import org.springframework.beans.factory.annotation.Value;
 import jakarta.annotation.PostConstruct;
 import java.security.Key;
 import java.time.Instant;
@@ -20,23 +20,33 @@ import java.util.Date;
 @Service
 public class TokenProvider {
     private static final Logger log = LoggerFactory.getLogger(TokenProvider.class);
-    private Key secretKey;
 
-    public TokenProvider() {
-    }
+    @Value("${SECRET_KEY}")
+    private String secretKeyString;
+    private Key secretKey;
 
     @PostConstruct
     public void init() {
-        this.secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        this.secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes());
     }
 
     public String create(UserEntity userEntity) {
         Date expiryDate = Date.from(Instant.now().plus(1L, ChronoUnit.DAYS));
-        return Jwts.builder().signWith(this.secretKey).setSubject(userEntity.getId()).setIssuer("demo app").setIssuedAt(new Date()).setExpiration(expiryDate).compact();
+        return Jwts.builder()
+                .signWith(this.secretKey)
+                .setSubject(userEntity.getId())
+                .setIssuer("demo app")
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .compact();
     }
 
     public String validateAndGetUserId(String token) {
-        Claims claims = (Claims)Jwts.parserBuilder().setSigningKey(this.secretKey).build().parseClaimsJws(token).getBody();
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(this.secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
         return claims.getSubject();
     }
 }
